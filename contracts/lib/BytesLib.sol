8a1dde1e83534432fb0816d83707aa910ea16093
peibin <wiki4zhang@gmail.com>
2024-03-08 10:06:46 +0800

    add comments on byteslib.sol

--- contracts/lib/BytesLib.sol
+++ contracts/lib/BytesLib.sol
@@ -69,4 +69,212 @@ library BytesLib {
         }
         return temBytes;
     }
+
+    function concatStorage(bytes storage _preBytes, bytes memory _postBytes) internal {
+        assembly {
+            // Read the first 32 bytes of _preBytes storage, which is the length
+            // of the array. (We don't need to use the offset into the slot
+            // because arrays use the entire slot.)
+            let fslot := sload(_preBytes.slot)
+            // Arrays of 31 bytes or less have an even value in their slot,
+            // while longer arrays have an odd value. The actual length is
+            // the slot divided by two for odd values, and the lowest order
+            // byte divided by two for even values.
+            // If the slot is even, bitwise and the slot with 255 and divide by
+            // two to get the length. If the slot is odd, bitwise and the slot
+            // with -1 and divide by two.
+            let slength := div(and(fslot, sub(mul(0x100, iszero(and(fslot, 1))), 1)), 2)
+            let mlength := mload(_postBytes)
+            let newlength := add(slength, mlength)
+            // slength can contain both the length and contents of the array
+            // if length < 32 bytes so let's prepare for that
+            // v. http://solidity.readthedocs.io/en/latest/miscellaneous.html#layout-of-state-variables-in-storage
+            switch add(lt(slength, 32), lt(newlength, 32))
+            case 2 {
+                // Since the new array still fits in the slot, we just need to
+                // update the contents of the slot.
+                // uint256(bytes_storage) = uint256(bytes_storage) + uint256(bytes_memory) + new_length
+                sstore(
+                    _preBytes.slot,
+                    // all the modifications to the slot are inside this
+                    // next block
+                    add(
+                        // we can just add to the slot contents because the
+                        // bytes we want to change are the LSBs
+                        fslot,
+                        add(
+                            mul(
+                                div(
+                                    // load the bytes from memory
+                                    mload(add(_postBytes, 0x20)),
+                                    // zero all bytes to the right
+                                    exp(0x100, sub(32, mlength))
+                                ),
+                                // and now shift left the number of bytes to
+                                // leave space for the length in the slot
+                                exp(0x100, sub(32, newlength))
+                            ),
+                            // increase length by the double of the memory
+                            // bytes length
+                            mul(mlength, 2)
+                        )
+                    )
+                )
+            }
+            case 1 {
+                // The stored value fits in the slot, but the combined value
+                // will exceed it.
+                // get the keccak hash to get the contents of the array
+                mstore(0x0, _preBytes.slot)
+                let sc := add(keccak256(0x0, 0x20), div(slength, 32))
+
+                // save new length
+                sstore(_preBytes.slot, add(mul(newlength, 2), 1))
+
+                // The contents of the _postBytes array start 32 bytes into
+                // the structure. Our first read should obtain the `submod`
+                // bytes that can fit into the unused space in the last word
+                // of the stored array. To get this, we read 32 bytes starting
+                // from `submod`, so the data we read overlaps with the array
+                // contents by `submod` bytes. Masking the lowest-order
+                // `submod` bytes allows us to add that value directly to the
+                // stored value.
+
+                let submod := sub(32, slength)
+                let mc := add(_postBytes, submod)
+                let end := add(_postBytes, mlength)
+                let mask := sub(exp(0x100, submod), 1)
+
+                sstore(
+                    sc,
+                    add(
+                        and(fslot, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff00),
+                        and(mload(mc), mask)
+                    )
+                )
+
+                for {
+                    mc := add(mc, 0x20)
+                    sc := add(sc, 1)
+                } lt(mc, end) {
+                    sc := add(sc, 1)
+                    mc := add(mc, 0x20)
+                } {
+                    sstore(sc, mload(mc))
+                }
+
+                mask := exp(0x100, sub(mc, end))
+
+                sstore(sc, mul(div(mload(mc), mask), mask))
+            }
+            default {
+                // get the keccak hash to get the contents of the array
+                mstore(0x0, _preBytes.slot)
+                // Start copying to the last used word of the stored array.
+                let sc := add(keccak256(0x0, 0x20), div(slength, 32))
+
+                // save new length
+                sstore(_preBytes.slot, add(mul(newlength, 2), 1))
+
+                // Copy over the first `submod` bytes of the new data as in
+                // case 1 above.
+                let slengthmod := mod(slength, 32)
+                let mlengthmod := mod(mlength, 32)
+                let submod := sub(32, slengthmod)
+                let mc := add(_postBytes, submod)
+                let end := add(_postBytes, mlength)
+                let mask := sub(exp(0x100, submod), 1)
+
+                sstore(sc, add(sload(sc), and(mload(mc), mask)))
+
+                for {
+                    sc := add(sc, 1)
+                    mc := add(mc, 0x20)
+                } lt(mc, end) {
+                    sc := add(sc, 1)
+                    mc := add(mc, 0x20)
+                } {
+                    sstore(sc, mload(mc))
+                }
+
+                mask := exp(0x100, sub(mc, end))
+
+                sstore(sc, mul(div(mload(mc), mask), mask))
+            }
+        }
+    }
+
+    /**
+     * examples
+     * 0000000000000000000000000000000000000000000000000000000000000014abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY1234567890000
+     * _start = 1 _length=3  result = cdefgh  
+     * bytes  mload()是load 32 字节 0000000000000000000000000000000000000000000000000000000000000014 
+     * 如果想包含我们要的数据那么就是0000000000000000000000000000000000000000000000000000000000000014abcdefgh
+     * 但是mload只能一次加载32字节，所以我们要相比较bytes 的起始位置 偏移 start+length 
+     * 在这里是 4个字节 
+     * 所以需要去掉最左边的8个0 : 00000000000000000000000000000000000000000000000000000014abcdefgh
+     * 
+     * 因为mc是有3个字节的0 相对比 0xc0 
+     * 所以mc还能放58位 也就是00000000000000000000000000000000000000000000000000000014ab
+     * 剩下的 cdefgh 就放在下一个 32字节了
+     */
+    function slice(bytes memory _bytes, uint256 _start, uint256 _length)  internal pure  returns (bytes memory) {
+        require(_length + 31 >= _length, "slice_overflow);
+        require(_bytes.length >= _start + _length , "slice_outOfBounds");
+        bytes memory tempBytes;
+        assembly {
+            switch iszero(_length)
+            case value {
+                temBytes := mload(0x40)
+
+                // if lengthmode == 0, it represents a multiple of 32
+                // else lengthmode == _length
+                let lengthmod := and(_length,31)
+                // add(temBytes, lengthmod) is all length 
+
+                // for examples
+                // 0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8 
+                // temBytes=0x80, _length=3, lengthmode=3
+                // mc := 0x80 + 3
+                // end := 0x80 + 3 + 3
+                let mc := add(add(tempBytes, lengthmod), mul(0x20, iszero(lengthmod)))
+                let end := add(mc, _length)
+
+                for {
+                    // The multiplication in the next line has the same exact purpose
+                    // as the one above.
+                    
+                    // for examples 
+                    // 0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8
+                    // temBytes=0x80, _length=3, lengthmode=3
+                    // mc := 0x80 + 3
+                    // end := 0x80 + 3 + 3
+                    //
+                    let cc := add(add(add(_bytes, lengthmod), mul(0x20, iszero(lengthmod))), _start)
+                } lt(mc, end) {
+                    // 32 bytes for one looper
+                    mc := add(mc, 0x20)
+                    cc := add(cc, 0x20)
+                } {
+                    mstore(mc, mload(cc))
+                }
+
+                mstore(tempBytes, _length)
+
+                //update free-memory pointer
+                //allocating the array padded to 32 bytes like the compiler does now
+                mstore(0x40, and(add(mc, 31), not(31)))
+            }
+            //if we want a zero-length slice let's just return a zero-length array
+            default { 
+                // load free pointer
+                temBytes := mload(0x40)
+                // store 0 at free pointer
+                mstore(temBytes,0)
+                // Although the slice has a length of 0, 
+                // bytes still has a length of 0x20 at the beginning. So the free pointer needs to add 0x20
+                mstore(0x40,add(temBytes,0x20))
+            }
+        }
+    }
 }
